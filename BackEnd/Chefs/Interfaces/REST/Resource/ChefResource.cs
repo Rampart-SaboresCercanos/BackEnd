094329c5cb1c4ba46fbe4012fd56343005d042da
sebastianrealcalderon <u20221d964@upc.edu.pe>
2024-11-21 16:55:12 -0500

    fix: deleted outdated chefs folder

--- BackEnd/Chefs/Interfaces/REST/Resource/ChefResource.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BackEnd.Chefs.Interfaces.REST.Resource;
-
-public record ChefResource(
-    int Id,
-    string Name,
-    double Rating,
-    bool IsFavorite,
-    string Gender
-);
---
+++ BackEnd/Dishes/Interfaces/ACL/IDishesContextFacade.cs
@@ -0,0 +1,8 @@
+﻿using BackEnd.Dishes.Domain.Model.Aggregates;
+
+namespace BackEnd.Dishes.Interfaces.ACL;
+
+public interface IDishesContextFacade
+{
+    Task<int> CreateDish(Dish dish);
+}
